98da3a172a4a7658d7712c570d29add78751646b
sheya <sheya@DESKTOP-KFPRLVU>
2022-08-22 12:39:55 +0900

    Add project files.

---
+++ MyDict/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace MyDict.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"host=localhost;port=5432;database=mydict;username=postgres;password=postgres");
+            //.LogTo(Console.WriteLine);
+        }
+            public DbSet<Word> words { get; set; }
+    }
+}
---
+++ MyDict/Word.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyDict.Data
+{
+    public class Word
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string EnglishWord { get; set; }
+
+        [StringLength(50)]
+        public string? Transcription { get; set; }
+        public string? Definition1 { get; set; }
+        public string? Definition2 { get; set; }
+        public string? Definition3 { get; set; }
+        public string? Definition4 { get; set; }
+        public string? Definition5 { get; set; }
+        public string? Example1 { get; set; }
+        public string? Example2 { get; set; }
+        public string? Example3 { get; set; }
+        public string? Example4 { get; set; }
+        public string? Example5 { get; set; }
+        public string? Translations { get; set; }
+        public DateTime NextReview { get; private set; }
+        public bool IsOnLearning { get; private set; }
+
+        public void CalculateNextReviewDate()
+        {
+            NextReview = DateTime.Now;
+        }
+
+        public void SetStatus()
+        {
+            IsOnLearning = true;
+        }
+    }
+}
