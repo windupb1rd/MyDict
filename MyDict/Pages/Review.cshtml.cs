fd12b2ce8f0a8f694cb298255f8e321fbfac0eaf
Aleksandr Sheianov <sheyan44@live.com>
2022-08-24 13:57:51 +0900

    start working on html

---
+++ MyDict/Pages/Review.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyDict.Pages
+{
+    public class ReviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
